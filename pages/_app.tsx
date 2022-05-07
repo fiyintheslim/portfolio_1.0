581efa0c13dff965ec95737ab4cb4500cf684994
fiyintheslim <fiyinoyekunle@gmail.com>
2022-05-07 23:42:58 +0100

    tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,47 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from "next/head"
+import {useEffect, useState} from "react"
+
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [isDarkTheme, setIsDarkTheme] = useState(false)
+
+  useEffect(()=>{
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      document.documentElement.classList.add('dark')
+      setIsDarkTheme(true)
+    } else {
+      document.documentElement.classList.remove('dark')
+      setIsDarkTheme(false)
+    }
+  }, [])
+  const handleTheme = ()=>{
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      document.documentElement.classList.add('dark')
+      localStorage
+      setIsDarkTheme(true)
+    } else {
+      document.documentElement.classList.remove('dark')
+      setIsDarkTheme(false)
+    }
+    setIsDarkTheme(!isDarkTheme)
+  }
+  return (
+  <>
+    <Head>
+      <link rel="icon" href="favicon.png" />
+    </Head>
+    
+    <Component {...pageProps} />
+    <button className="" onClick={()=>handleTheme()}>
+    {isDarkTheme ?
+        <span aria-label="Light mode" role="img">🌞</span> :
+        <span aria-label="Dark mode" role="img">🌜</span>}
+    </button>
+  </>
+  )
 }
 
 export default MyApp
---
+++ styles/globalStyles.ts
@@ -0,0 +1,10 @@
+
+
+export const lightTheme = {
+    body: '#f1f1f1',
+    text: '#121620'
+  };
+export const darkTheme = {
+    body: '#121620',
+    text: '#f1f1f1'
+  };
