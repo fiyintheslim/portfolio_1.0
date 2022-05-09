cd6e240bd018114131fd0c49a24a593fcabfd5ea
fiyintheslim <fiyinoyekunle@gmail.com>
2022-05-09 10:21:40 +0100

    menu

--- layouts/Header.tsx
+++ layouts/Header.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
 import Link from "next/link"
 import style from "../styles/scss/header.module.scss"
 
 const Header = () => {
+  const [menu, setMenu] = useState(false);
+  const menuEle = useRef<HTMLDivElement>(null);
+  const handleMenu = () => {
+    if(menuEle.current){
+      if(menu){
+        menuEle.current.style.transform = "translateX(100vw)"
+        menuEle.current.style.opacity = "0"
+      }else{
+        menuEle.current.style.transform = "translateX(0vw)"
+        menuEle.current.style.opacity = "1"
+      }
+      setMenu(!menu)
+    }
+  }
   return (
+    <>
     <header className={`shadow-lg ${style.header}`}>
     <div className={`container ${style.container__flex}`}>
         <p className={`${style.logo}`}>
@@ -12,16 +27,38 @@ const Header = () => {
             </Link>
         </p>
         <div className={`${style.hamburger}`}>
-            <p className={style.hamburger__text}>MENU</p>
-            <div className={`${style.hamburger__btn}`}>
-            
-                <span className={`bg-slate-400 ${style.line1} ${style.line}`}></span>
-                <span className={`bg-blue-500 ${style.line2} ${style.line}`}></span>
-                <span className={`bg-slate-400 ${style.lin3} ${style.line}`}></span>
+            <p className={`text-slate-300 ${style.hamburger__text} ${style.hamburger__menu}`}>MENU</p>
+            <div onClick={handleMenu} className={`${style.hamburger__btn}`}>
+                <span className={`bg-slate-400 ${style.line1} ${style.line} one`}></span>
+                <span className={`bg-blue-500 ${style.line2} ${style.line} two`}></span>
+                <span className={`bg-slate-400 ${style.lin3} ${style.line} three`}></span>
             </div>
       </div>
     </div>
+    
+    
   </header>
+  <div ref={menuEle} className={`${style.menu}`}>
+    <div className={`bg-slate-300 dark:bg-slate-900 ${style.menu__content}`}>
+      <svg onClick={handleMenu} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className={`fill-slate-700 dark:fill-slate-300 ${style.close}`} viewBox="0 0 16 16">
+        <path fillRule="evenodd" d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z"/>
+        <path fillRule="evenodd" d="M2.146 2.146a.5.5 0 0 0 0 .708l11 11a.5.5 0 0 0 .708-.708l-11-11a.5.5 0 0 0-.708 0Z"/>
+      </svg>
+      <Link  href="/" >
+        <p onClick={handleMenu} className={`text-slate-700 dark:text-slate-300 ${style.hamburger__text} ${style.link}`}>HOME</p>
+      </Link>
+      <Link href="/projects">
+        <p onClick={handleMenu} className={`text-slate-700 dark:text-slate-300 ${style.hamburger__text} ${style.link}`}>PROJECTS</p>
+      </Link>
+      <Link href="/about">
+        <p onClick={handleMenu} className={`text-slate-700 dark:text-slate-300 ${style.hamburger__text} ${style.link}`}>ABOUT</p>
+      </Link>
+      <Link href="/contact" >
+        <p onClick={handleMenu} className={`text-slate-700 dark:text-slate-300 ${style.hamburger__text} ${style.link}`}>CONTACT</p>
+      </Link>
+    </div>
+  </div>
+  </>
   )
 }
 
