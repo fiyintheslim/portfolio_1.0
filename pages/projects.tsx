927e8e98ed191e9d02cd96c38811b720c7360a0a
fiyintheslim <fiyinoyekunle@gmail.com>
2022-05-10 19:19:28 +0100

    complete, animation next

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ProjectCard from "../components/ProjectCard"
-import {rdLand, TheCacophone} from "../utilities/projects"
+import {rdLand, TheCacophone, bibli, easyAtRents} from "../utilities/projects"
 import style from "../styles/scss/projects.module.scss"
 
+const projects = [rdLand, TheCacophone, bibli, easyAtRents]
 const Projects = () => {
   return (
     <main>
       <div className={`container-custom container--vertical--spacing`}>
         <h3 className={`header-1`}>My Projects</h3>
         <div className={`${style.projects}`}>
-          <ProjectCard project={rdLand} />
-          <ProjectCard project={TheCacophone} />
+          {projects.map((project, i)=><ProjectCard key={i} project={project} />)}
           <a rel="noreferrer" href="https://github.com/fiyintheslim?tab=repositories" target="_blank" className={`btn `}>View More Projects</a>
         </div>
         
--- utilities/projects.ts
+++ utilities/projects.ts
@@ -2,7 +2,11 @@ const temp = {
     name:"",
     src:"",
     desc:"",
-    tech:[]
+    tech:[],
+    links:{
+        github:"",
+        live:""
+    }
 }
 
 export const rdLand  = {
@@ -24,5 +28,27 @@ export const TheCacophone = {
     links:{
         github:"https://github.com/fiyintheslim/node-chat-app",
         live:"http://thecacophone.herokuapp.com"
+    }
+}
+
+export const bibli = {
+    name:"Bibli",
+    src:"/projects/bibli.png",
+    desc:"A CRUD app for users to upload books ebooks, read and leave reviews. It contains: paginations and search functionality.",
+    tech:["react", "redux", "react-router-dom", "Bootstrap", "JWT", "express", "mongodb"],
+    links:{
+        github:"",
+        live:"http://bibli2.herokuapp.com"
+    }
+}
+
+export const easyAtRents = {
+    name:"Easy@rents",
+    src:"/projects/easyatrents.png",
+    desc:"A CRUD application for property listing. Users create an account, upload property data",
+    tech:["html", "css", "javascript", "laravel", "leaflet"],
+    links:{
+        github:"",
+        live:"http://easyatrents.herokuapp.com"
     }
 }
