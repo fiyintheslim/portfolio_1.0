5e1f6429d769ff4895442e11ec8ee6c495803c84
fiyintheslim <fiyinoyekunle@gmail.com>
2022-05-10 03:05:43 +0100

    about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -19,21 +19,21 @@ const About = () => {
       <div className={`container-custom container--vertical--spacing`}>
         <h3 className={`header-1 `}>About me</h3>
         <p className={`content ${style.paragraph}`}>
-          Hi, I'm Oyekunle Fiyinfoluwa, a web developer with my focus currently in the frontend.
-           I enjoy creating software that works and debugging (The dopamine rush after squashing the bug is amazing). I can't put a date to when I first picked interest in computers
-           but I took my first step, after previous first steps, to becoming a developer in 2020 when I read O'Reilly's Head First HTML and CSS during the covid-19 lockdown,
+          Hi, I&apos;m Oyekunle Fiyinfoluwa, a web developer with my focus currently in the frontend.
+           I enjoy creating software that works and debugging (The dopamine rush after squashing the bug is amazing). I cannot put a date to when I first picked interest in computers
+           but I took my first step, after previous first steps, to becoming a developer in 2020 when I read Head First HTML and CSS during the covid-19 lockdown,
             fastforward to today and I still am mind blown by the possibilities that can be created through computers.
         </p>
         <p className={`content ${style.paragraph}`}>
           I started off on Fiverr where I built a few websites for people using wix.
            Then I have built apps using various technologies which I had to learn in the moment to achieve the expected results while freelancing. 
            Technologies like: shopify liquid, solidity, php & laravel etc.
-           Now I'm focusing all my energy into building performant and aesthetically pleasing UI.
+           Now I am focusing all my energy into building performant and aesthetically pleasing UI.
            I primarily use React.js/Next.js and javascript/typescript.
         </p>
         <p className={`content ${style.paragraph}`}>
-          When I'm not working, I play video games, watch football matches (Manchester united 😪), read books especially on the human condition (Viktor Frankl, Mark Manson and Robert Greene).
-          I also enjoy watching movies especially superhero fiction genres and in my opinion I believe having the ability to get the computer to do exactly what's in mind is a super power. 
+          When I am not working, I play video games, watch football matches (Manchester united 😪), read books especially on the human condition (Viktor Frankl, Mark Manson and Robert Greene).
+          I also enjoy watching movies especially superhero fiction genres and in my opinion I believe having the ability to get the computer to do exactly what is in mind is a super power. 
         </p>
       </div>
       <div className={`container-custom container--vertical--spacing `}>
