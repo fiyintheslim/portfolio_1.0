b08532322cffdfa17b8a73dd90a19084f59a8b47
fiyintheslim <fiyinoyekunle@gmail.com>
2022-05-14 10:34:51 +0100

    Colors

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -23,13 +23,13 @@ const ComponentCard = ({project} : Props) => {
       <div className={`${style.info}`}>
         <h3 className={`header-2 ${style.title}`}>{name}</h3>
         <p className={`content-2 ${style.description} bg-inherit lg:bg-slate-500 w-full rounded lg:p-8 max-w lg:max-w-xl dark:lg:bg-slate-900`}>{desc}</p>
-        <p className={`${style.technologies} text-slate-500 text-lg font-bold`}>
+        <p className={`${style.technologies} text-slate-900 dark:text-slate-300 text-lg font-bold`}>
           {tech.map((t, i)=><span key={i} className={`${style.tech}`}>{t}</span>)}
         </p>
         <div className={`${style.iconsContainer}`}>
           {links.github &&
           <a rel="noreferrer" target="_blank" href={links.github}>
-            <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={`feather feather-github ${style.icon} stroke-slate-500 hover:stroke-blue-600`}>
+            <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={`feather feather-github ${style.icon} stroke-slate-700 dark:stroke-slate-300 hover:stroke-blue-600`}>
               <title>GitHub</title>
               <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
             </svg>
@@ -37,7 +37,7 @@ const ComponentCard = ({project} : Props) => {
           }
           {links.live &&
             <a rel="noreferrer" target="_blank" href={links.live}>
-              <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={`feather feather-external-link ${style.icon} stroke-slate-500 hover:stroke-blue-600`}>
+              <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={`feather feather-external-link ${style.icon} stroke-slate-700 dark:stroke-slate-300 hover:stroke-blue-600`}>
                 <title>External Link</title>
                 <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
                 <polyline points="15 3 21 3 21 9"></polyline>
