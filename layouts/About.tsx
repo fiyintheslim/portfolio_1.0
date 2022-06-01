ea64920ca896a6e8bdc1a7e129c2c5581d6cdaae
fiyintheslim <fiyinoyekunle@gmail.com>
2022-06-01 17:35:27 +0100

    content

--- layouts/About.tsx
+++ layouts/About.tsx
@@ -14,10 +14,11 @@ const About = () => {
           </h4>
           <p className={`content`}>
           I create performant web applications and provide amazing user experiences using Javascript and Typescript.
-          I use React as my primary frontend library for making web apps as well as other technologies like: jquery, tailwindCSS, nextJS, postgresQL, redux.
-          I am currently concentrating on accessibility, performance, and the creation of immersive aesthetic experiences through design and animations. 
+          I use React as my primary frontend library for making web apps as well as other technologies like: CSS, jquery, tailwindCSS, nextJS, postgresQL, redux, mongoDB and node.
+          
           </p>
           <p className={`content`}>
+            I am currently concentrating on accessibility, performance, and the creation of immersive aesthetic experiences through design and animations. 
             My strengths are my ability to conceptualize and execute on projects from start to end, my problem solving skills, my ability to learn quickly and my ability to communicate my ideas.
 
           </p>
